b0f16bc2eafc1bc6ca1f5bcd38ee3aad1ee4d052
stayFoolish <nemanjakaraklajic90@icloud.com>
2023-08-31 20:58:46 +0200

    Update: Main page responsive corrections

--- src/components/Entry/Entry.jsx
+++ src/components/Entry/Entry.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {NavLink} from "react-router-dom";
-
+import './entry.css'
 const Entry = () => {
   return (
     <div className="entry__container scale-up-center">
